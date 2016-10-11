162e020def87dca809be3f031e473cdbf23845f7
Lucas Sales <saless182@gmail.com>
2016-10-11 20:47:47 -0300

    Strings for pt-Br Added some values to MovieDetailViewModel Up navigation for MovieDetailActivity

--- app/src/main/java/com/moviedatabase/MovieDetailFragment.java
+++ app/src/main/java/com/moviedatabase/MovieDetailFragment.java
@@ -24,13 +24,11 @@ import java.util.Locale;
 public class MovieDetailFragment extends Fragment implements MovieDetailPresenterListener {
     public static final String TAG = MovieListFragment.class.getSimpleName();
     private static final String MOVIE_KEY = "movie";
-    private ViewDataBinding dataBinding;
     private MovieDetailPresenter presenter;
     private MovieDto movieDto;
     private MovieDetailViewModel viewModel;
 
     public static MovieDetailFragment newInstance(MovieDto movieDto) {
-
         MovieDetailFragment fragment = new MovieDetailFragment();
         Bundle args = new Bundle();
         args.putParcelable(MOVIE_KEY, movieDto);
@@ -48,18 +46,19 @@ public class MovieDetailFragment extends Fragment implements MovieDetailPresente
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
+        ViewDataBinding dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
+        View rootView = dataBinding.getRoot();
         viewModel = new MovieDetailViewModel(movieDto);
         dataBinding.setVariable(BR.movie, viewModel);
         presenter.fetchMovieDetails();
-        return dataBinding.getRoot();
+        return rootView;
     }
 
     @Override
     public void onMovieDetailsLoaded(MovieDetailsDto movieDetailsDto) {
-        if (!movieDetailsDto.original_language.equals(Locale.getDefault().getLanguage())) {
-            viewModel.setOriginalTitle(String.format("(%s)", movieDetailsDto.original_title));
-            viewModel.setRuntime(movieDetailsDto.runtime);
+        if (!movieDetailsDto.getOriginal_language().equals(Locale.getDefault().getLanguage()) && !movieDetailsDto.getTitle().equals(movieDetailsDto.getOriginal_title())) {
+            viewModel.setOriginalTitle(movieDetailsDto.getOriginal_title());
         }
+        viewModel.setRuntime(movieDetailsDto.getRuntime());
     }
 }
