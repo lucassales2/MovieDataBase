9f17513b09360b2ff9942acb7b7dfb721ea98fc2
Lucas Sales <saless182@gmail.com>
2016-10-19 19:17:37 -0200

    Sync module

---
+++ app/src/main/java/com/moviedatabase/sync/MovieSyncService.java
@@ -0,0 +1,24 @@
+package com.moviedatabase.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MovieSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static MovieSyncAdapter sMovieSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        synchronized (sSyncAdapterLock) {
+            if (sMovieSyncAdapter == null) {
+                sMovieSyncAdapter = new MovieSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sMovieSyncAdapter.getSyncAdapterBinder();
+    }
+}
