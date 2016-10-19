f6c917587a852319174d63bf277b82f6ca999085
Lucas Sales <saless182@gmail.com>
2016-10-19 19:32:17 -0200

    Prepering to add support for tablets

--- app/src/main/java/com/moviedatabase/MainActivity.java
+++ app/src/main/java/com/moviedatabase/MainActivity.java
@@ -1,10 +1,15 @@
 package com.moviedatabase;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.moviedatabase.sync.MovieSyncAdapter;
+
+public class MainActivity extends AppCompatActivity implements MovieListFragment.Callback {
+
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (findViewById(R.id.movie_detail_container) != null) {
+            // The detail container view will be present only in the large-screen layouts
+            // (res/layout-sw600dp). If this view is present, then the activity should be
+            // in two-pane mode.
+            mTwoPane = true;
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.movie_detail_container, new MovieDetailFragment(), MovieDetailFragment.TAG)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+        }
+        MovieListFragment movieListFragment = (MovieListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_movie_listing);
+        movieListFragment.setTwoPane(mTwoPane);
+        MovieSyncAdapter.initializeSyncAdapter(this);
+    }
+
+    @Override
+    public void onItemSelected(Uri dateUri) {
+
     }
 }
